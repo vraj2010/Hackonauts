4ca8b3d137d3d5111172eb36874c842f94b5754d
Vraj Patel <105788673+vraj2010@users.noreply.github.com>
2025-01-09 00:11:32 +0530

    main.py

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ def run_flow(message: str) -> dict:
 
 
 # Streamlit Interface
-st.set_page_config(page_title="Hackonauts Chatbot", page_icon="🧑🏻‍💻", layout="centered")
+st.set_page_config(page_title="Hackonauts Chatbot", layout="centered")
 
-st.title("💬 Chat with Hackonauts")
+st.title("Chat with Hackonauts")
 st.markdown("Get instant responses from LangFlow's AI engine.")
 
 # Input container
